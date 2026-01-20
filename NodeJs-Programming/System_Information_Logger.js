e8d162bb0ee644812190a7ab200df25265cb4a2c
AkshatJain01 <168081234+AKHI004@users.noreply.github.com>
2026-01-20 14:58:18 +0530

    NodeJs-Programming practice code

--- NodeJs-Programming/System_Information_Logger.js
+++ NodeJs-Programming/System_Information_Logger.js
@@ -1,6 +1,5 @@
 const os = require("os");
 const fs = require("fs");
-
 function logSystemInfo() {
   const info = `
 Time: ${new Date().toISOString()}
@@ -11,5 +10,4 @@ Memory: ${(os.totalmem() / (1024 * 1024)).toFixed(2)} MB
 
   fs.appendFile("system_log.txt", info, () => {});
 }
-
 setInterval(logSystemInfo, 5000);
